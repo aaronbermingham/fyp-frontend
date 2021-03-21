0014424bc327df59f838eca804d711f1b2941479
Aaron Bermingham <aaronbrmnghm@gmail.com>
2021-03-21 14:34:05 +0000

    Users can choose table to book, filter tables that user can see by enabled/disabled

--- src/components/UserTableComponent.js
+++ src/components/UserTableComponent.js
@@ -3,6 +3,9 @@ import TableService from '../services/TableService';
 import Modal from "react-bootstrap/Modal";
 import Moment from 'moment';
 import Button from "react-bootstrap/Button";
+import UserService from '../services/UserService';
+import AuthService from "../services/AuthService";
+import { withRouter } from 'react-router-dom';
 
 
 class UserTableComponent extends Component {
@@ -13,25 +16,35 @@ class UserTableComponent extends Component {
         this.state = {
             tables: [],
             numOfSeats: 0,
-            modal: false
+            modal: false,
+            tableId: 0,
 
         }
         this.onClick = this.onClick.bind(this);
+        this.createBooking = this.createBooking.bind(this);
+        // this.filterTables = this.filterTables.bind(this);
     }
 
     componentDidMount() {
-        let date = this.props.bookingTime;
+        const user = AuthService.getCurrentUser();
+        if (user) {
+            this.setState({
+              id: user.id,
+              currentUser: user,
+              businessUser: user.roles.includes("ROLE_BUSINESS"),
+            });
+          }
+        let date = this.props.time;
         let utc = Moment.utc(date).format();
+
         TableService.getTables().then((res) => {
-            this.setState({ tables: res.data })
+            this.setState({ tables: res.data.filter(table => table.disabled === false) })
             this.setState({ numOfSeats: res.data.numSeats })
             console.log("Table info ", res.data)
             for (let i = 0; i < res.data.length; i++) {
                 console.log((i+1)+ " Table reslist ", res.data[i].resList);
               }
               console.log("Passed date ", utc)
-           
-
         });
         TableService.getSeats().then((res) => {
             this.setState({ numOfSeats: res.data })
@@ -39,22 +52,90 @@ class UserTableComponent extends Component {
             console.log(this.state.numOfSeats);
 
         });
-
-
-
+        
     }
+
+    // filterTables(){
+    //     this.setState({tables:tables.filter(table => table.disabled === false)})
+    //     console.log(this.state.tables);
+    // }
 
     onClick(table) {
         console.log("Table ", table.id)
+        this.setState({tableId: table.id})
     }
 
-    render() {
+    createBooking = (e) => {
+        //BookingService.clearBookings();
+        const locale = 'en';
+       
+        e.preventDefault();
+        let booking = {
+          date: this.props.date.toISOString().slice(0, 10),
+          time: this.props.time.toLocaleTimeString(locale, { hour: 'numeric', hour12: false, minute: 'numeric', second: 'numeric' }),
+          numGuests: this.props.guests
+        };
+        console.log('booking => ' + JSON.stringify(booking));
+        UserService.addBookingTable(booking, this.state.id, this.state.tableId).then(res => {
+            console.log("res data id ",res.data)
+            this.bId = res.data;
+          console.log("Booking id ", this.bId)
+          if(this.bId === -1){
+            alert("There are no tables available that can accomodate your party");
+          }
+          else if(this.bId === -2){
+            alert("There are no tables available at your chosen time");
+          }
+          else if(this.bId > 0){
+            this.props.history.push(`/orderFood/${this.bId}`);
+          }
+        })
+    
+       
+      }
 
+    render() {
+        const utc = Moment.utc(this.props.time).format();
         return (
 
             <div>
                  <h2 className="text-center">Tables</h2>
-                 Data from parent is: {Moment.utc(this.props.bookingTime).format()}
+                 Data from parent is: {Moment.utc(this.props.time).format()}
+                 <table className="table table-striped table-bordered">
+                 <tbody>
+                            {
+                                this.state.tables.map(
+                                    table1 =>
+                                        <tr key={table1.id}>
+                                            <td>{table1.id}</td>
+                                            <td>
+                                                    {
+                                                        table1.resList.map((subitem, i) => {
+                                                            return (
+                                                                <li>{subitem.endBooking +"Z" === utc? "Yes" : "No" }</li>
+                                                                // <td> {Moment.utc(this.props.bookingTime).format()===subItem.endBooking? "Yes" : "No"}</td>
+
+                                                            )
+                                                           
+                                                        })
+                                                    }</td>
+                                                     <td>
+                                                    {
+                                                        table1.resList.map((subitem, i) => {
+                                                            return (
+                                                                <li>{subitem.endBooking + "Z"}</li>
+                                                                // <td> {Moment.utc(this.props.bookingTime).format()===subItem.endBooking? "Yes" : "No"}</td>
+
+                                                            )
+                                                           
+                                                        })
+                                                    }</td>
+                                                    <td> {utc}</td>
+                                                    
+                                        </tr>
+                                )
+                            }
+                        </tbody></table>
                 <div className="container">
 
 
@@ -67,9 +148,11 @@ class UserTableComponent extends Component {
 
 
                                 {
-                                    this.state.tables.filter(table => table.disabled === false).map(
+                                    this.state.tables.map(
+                                        
                                         table =>
-
+                                    //    {tables.filter(table => table.resList.endBooking !== utc+2)}
+                                        
                                             <div
 
                                                 style={{
@@ -116,6 +199,7 @@ class UserTableComponent extends Component {
 
                                                 <span> Seats: {table.numSeats}</span>
                                                 <span></span>
+                                                
 
 
 
@@ -132,13 +216,40 @@ class UserTableComponent extends Component {
                                 }
 
                             </tbody>
+                   
 
                         </div>
 
-
+                        <button className="btn btn-primary" onClick={this.createBooking}>Add booking</button>
                     </table>
 
                 </div>
+                {/* <div>
+                <table className="table table-striped table-bordered">
+                            {
+                                this.state.tables.map(
+                                    table1 =>
+                                        <tr key={table1.id}>
+                                            <td>{table1.id}</td>
+                                            <td>
+                                                    {
+                                                        table1.resList.map((subitem, i) => {
+                                                            if(subitem.endBooking+"Z" !== utc || table1.id !== subitem.tableId)
+                                                            return (
+                                                                <td>Table id {table1.id} End booking {subitem.endBooking} time {utc} Res table id {subitem.tableId}</td>
+                                                                // <td> {Moment.utc(this.props.bookingTime).format()===subItem.endBooking? "Yes" : "No"}</td>
+
+                                                            )
+                                                           
+                                                        })
+                                                    }</td>
+                                                
+                                                    
+                                        </tr>
+                                )
+                            }
+                        </table>
+                            </div> */}
 
                 {/* <div className="row">
                     <table className="table table-striped table-bordered">
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authHeader from './AuthHeader';
 
 const API_URL = 'http://localhost:8080/api/auth/';
-const BOOKING_URL = "http://localhost:8080/api/user/"
+const BOOKING_URL = "http://localhost:8080/api/auth/user/"
 
 class UserService {
   getPublicContent() {
@@ -23,24 +23,29 @@ class UserService {
 
   getUsers() {
     return axios.get(API_URL + "users", { headers: authHeader() });
-}
+  }
 
-getUserById(userId) {
-  return axios.get(API_URL + "userById/" + userId, { headers: authHeader() });
-}
+  getUserById(userId) {
+    return axios.get(API_URL + "userById/" + userId, { headers: authHeader() });
+  }
 
-updateUser(user, userId) {
-  return axios.put(API_URL + "updateUser/" + userId, user, { headers: authHeader() });
-}
+  updateUser(user, userId) {
+    return axios.put(API_URL + "updateUser/" + userId, user, { headers: authHeader() });
+  }
 
-addBooking(booking, userId) {
-  const user = JSON.parse(localStorage.getItem('user'));
-  const token = user.accessToken;
+  addBooking(booking, userId) {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const token = user.accessToken;
+    console.log("Token ", token);
+    return axios.post(BOOKING_URL + "addBooking/" + userId, booking, { headers: authHeader() });
+  }
 
-  // const token = this.getCurrentUser().accessToken;
-  console.log("Token ", token);
-  return axios.post(BOOKING_URL + "addBooking/" + userId, booking, { headers: authHeader() });
-}
+  addBookingTable(booking, userId, tableId) {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const token = user.accessToken;
+    console.log("Token ", token);
+    return axios.post(BOOKING_URL + "addBookingTable/" + userId + "/" +tableId, booking, { headers: authHeader() });
+  }
 
 }
 
