af11b1485b182981aaa98015e40ed399c6cf5a23
Aaron Bermingham <aaronbrmnghm@gmail.com>
2021-04-28 21:25:05 +0100

    All items line chart

--- src/services/ContactTracingService.js
+++ src/services/ContactTracingService.js
@@ -4,8 +4,8 @@ const TRACE_API_BASE_URL = "http://localhost:8080/api/auth/tracking/"
 
 class ContactTracingService {
     // Methods for booking items
-    getTrackingList(booking){
-        return axios.post(TRACE_API_BASE_URL + "getTrackingHistory", booking,{ headers: authHeader() });
+    getTrackingList(staffShift){
+        return axios.post(TRACE_API_BASE_URL + "getTrackingHistory", staffShift,{ headers: authHeader() });
     }
 
     sendTrackingEmail(contact){
